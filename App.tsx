10c955b8405c173d0871314418ba10d4c62c8027
phelpsdevelops <raquanp78@icloud.com>
2026-04-03 11:17:50 -0400

    added react native gesture handler, confirmed user swipe direction detection in the consol logs

--- App.tsx
+++ App.tsx
@@ -1,20 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import Game from "./src/componets/Game";
+const App=()=> <GestureHandlerRootView style={{flex:1}}>
+  <Game/>
+  </GestureHandlerRootView>;
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
--- src/componets/Game.tsx
+++ src/componets/Game.tsx
@@ -1,11 +1,50 @@
 import * as React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 import {  SafeAreaView } from 'react-native-safe-area-context';
+import { Colors } from '../styles/colors';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 
 export default function Game():React.JSX.Element{
+    const handleGesture =(event: any) =>
+    {
+
+    }
+    const pan=Gesture.Pan().minDistance(20).onEnd((event)=>{
+        console.log(event)
+        const {translationX,translationY} = event;
+
+        //decide whether swipe was mostly horizontal or vertical
+          if (translationX > 30) {
+        //  handleSwipe("right");
+        console.log("right");
+        } else if (translationX < -30) {
+        //  handleSwipe("left");
+        console.log("left");
+        }
+        else {
+        if (translationY > 30) {
+        //  handleSwipe("down");
+        console.log("down");
+        } else if (translationY < -30) {
+         // handleSwipe("up");
+         console.log("up");
+        }
+      }
+    });
     return(
-        <SafeAreaView>
+       <GestureDetector gesture={pan}>
+        <SafeAreaView style={styles.container} >
 
         </SafeAreaView>
+        </GestureDetector>
     )
 
 }
+const styles= StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:Colors.primary,
+    }
+
+})
