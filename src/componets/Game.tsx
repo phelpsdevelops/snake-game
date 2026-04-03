cbfb9581300284ec88e8352e0e060b5a0081463e
phelpsdevelops <raquanp78@icloud.com>
2026-04-03 13:15:07 -0400

    added snake move function triggered by useEffect to allow snake movement as long as the game is not paused or over

--- src/componets/Game.tsx
+++ src/componets/Game.tsx
@@ -25,6 +25,49 @@ export default function Game():React.JSX.Element{
     )
     const [isGameOver,setIsGameOver]= React.useState<boolean>(false);
     const [isPaused,setIsPaused]= React.useState<boolean>(false);
+    React.useEffect(()=>{
+        // move the snake if the game is not over 
+            if(!isGameOver){
+                //move every 50 milliseconds if the game is not paused 
+                const intervalId= setInterval(()=>{
+                    !isPaused && moveSnake();
+                },MOVE_INTERNAL)
+                return ()=> clearInterval(intervalId);
+            }
+    },[snake, isGameOver,isPaused])
+
+    const moveSnake = ()=>{
+        //grab the snake head thats in the array of the snake at position 0
+        const snakeHead=snake[0];
+        // cant just do snakeHead.x=10 because we are using react... we will have to create a copy
+        const newHead={...snakeHead}// create a copy ( we will replace the state by passing the newHead)
+
+        //game over 
+
+
+        //switch case to determine which way the snake will go.
+        switch(direction){
+            case Direction.Up:
+                newHead.y -=1;
+                break;
+            case Direction.Down:
+                newHead.y +=1;
+                break;
+            case Direction.Left:
+                newHead.x -=1;
+                break;
+            case Direction.Right:
+                newHead.x +=1;
+                break;
+                default:
+                    break;
+
+        }
+        //we need to remove the last position of the head so that snake does not continually grow without eating so we will use the slice function
+        //slice is used like this slice(start,end) start=where to begin end=where to stop, but not include
+            setSnake([newHead,...snake.slice(0,-1)])
+
+    }
    const handleGesture = (event: GestureEventType) => {
   const { translationX, translationY } = event;
 
@@ -60,17 +103,16 @@ const styles= StyleSheet.create({
     container:{
         flex:1,
         backgroundColor:Colors.primary,
-        paddingTop:5
+        
     },
     boundaries:{
         flex:1,
         borderColor: Colors.primary,
-        borderWidth:15,
-        
-        borderBottomLeftRadius:30,
-        borderBottomRightRadius:30,
-        borderTopRightRadius:30,
-        borderTopLeftRadius:30,
+        borderWidth:35,
+        borderBottomLeftRadius:50,
+        borderBottomRightRadius:50,
+        borderTopRightRadius:50,
+        borderTopLeftRadius:50,
         backgroundColor:Colors.background,
     }
 
