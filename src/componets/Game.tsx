6cc65c3a781cbb1a997dedc3b43fbef128bb051f
phelpsdevelops <raquanp78@icloud.com>
2026-04-03 10:44:26 -0400

    initial commit for snake game with folders that will hold game utilities and components

---
+++ src/componets/Game.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import {  SafeAreaView } from 'react-native-safe-area-context';
+
+export default function Game():React.JSX.Element{
+    return(
+        <SafeAreaView>
+
+        </SafeAreaView>
+    )
+
+}
